66926d0143c0d131e4335820ed546906852c8128
Ayush <ayushnita.ece@gmail.com>
2018-08-02 00:38:09 +0530

    Set Buy component and Sell Component according to Home Page

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {take, tap} from 'rxjs/operators';
+import {pipe, Observable} from 'rxjs';
+import {buy} from '../module/buy';
 
 @Component({
   selector: 'app-buy',
@@ -10,9 +13,9 @@ export class BuyComponent implements OnInit {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
 
   constructor(private http : HttpClient){}
-  buyDetails : any = [];
+  buyDetails ;
   ngOnInit() {
-       this.buyDetails = this.http.get('../../assets/sample-buy-online.json');
+    this.buyDetails =  this.http.get<buy>('../../assets/sample-buy-online.json');
   }
 
 }
---
+++ src/app/module/buy.ts
@@ -0,0 +1,6 @@
+export interface buy{
+        seller: String;
+        Paymentmethod: String;
+        limit : String;
+        PricePerSteem : String;
+} 
