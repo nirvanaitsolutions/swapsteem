d5592fc5fe23b549cc2e5b2961e0d5ff6815511d
Ayush <ayushnita.ece@gmail.com>
2018-08-07 23:37:10 +0530

    Chage buy model to order model

--- src/app/module/buy.ts
+++ src/app/module/buy.ts
@@ -1,6 +1,13 @@
 export interface buy{
-        seller: String;
-        Paymentmethod: String;
-        limit : String;
-        PricePerSteem : String;
+        ad_id: String;
+        createdby : String;
+        createdfor : String;
+        order_type: String;
+        order_coin_amount: String;
+        order_coin : String;
+        order_rate : String;
+        order_payment_method : String;
+        agree_terms : Boolean;
+        country : String;
+        currency : String;
 } 
