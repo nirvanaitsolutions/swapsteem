44321db5a9b7912de856e16a741ebda212fa1ff3
Ayush <ayushnita.ece@gmail.com>
2018-08-13 20:52:53 +0530

    Wallet done

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OrderResponse } from '../module/order';
 import { APIService } from '../../service/api.service';
+import {tap} from 'rxjs/operators';
 import { Router } from '@angular/router';
+import {SteemconnectAuthService} from '../steemconnect/services/steemconnect-auth.service';
 
 @Component({
   selector: 'app-wallet',
@@ -11,14 +13,26 @@ import { Router } from '@angular/router';
 })
 export class WalletComponent implements OnInit {
 
-  constructor(private apiSer : APIService,
-    private router : Router) { }
+  userData :any =[];
+
+  constructor( private _auth: SteemconnectAuthService,
+              private apiSer : APIService,
+               private router : Router) { }
   openOrders : Observable<OrderResponse[]> ;
   closedOrders : Observable<OrderResponse[]>;
 
   ngOnInit() {
-    this.openOrders = this.apiSer.getOpenOrdersForUser('aneilpatel');
-    this.closedOrders = this.apiSer.getClosedOrdersForUser('aneilpatel');
+    
+    this.userData = this._auth.getUserData().subscribe( data => {
+      this.userData = data;
+      this.openOrders = this.apiSer.getOpenOrdersForUser(this.userData.name);
+      this.closedOrders = this.apiSer.getClosedOrdersForUser(this.userData.name);
+    });
+
+    // this.userData = this._auth.getUserData().pipe(
+    //                                         tap(data =>  )
+    // )
+    
   }
 
 }
