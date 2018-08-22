e97c435395d55dad41fb0bdfb905eda6af56e36b
aneilpatel05 <anil.patel.gty@gmail.com>
2018-08-22 01:31:15 -0700

    removed hardcoded values

--- src/service/websockets.service.ts
+++ src/service/websockets.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import {WebSocketSubject,WebSocketSubjectConfig} from 'rxjs/observable/dom/WebSocketSubject';
 import * as busy from 'busyjs'
+import { SteemconnectAuthService } from '../app/steemconnect/services/steemconnect-auth.service';
+
+export interface OAuth2Token {
+  access_token: string;
+  expires_in: number;
+  username: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +15,25 @@ import * as busy from 'busyjs'
 export class WebsocketsService {
   public busy = new busy.Client('wss://api.busy.org');;
 
-  constructor() {
+  constructor(private auth :SteemconnectAuthService) {
     
     this.busy.call('get_notifications', ['aneilpatel'], function(err, result) {
       console.log(err, result);
     })
-    this.busy.call('login', ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBwIiwicHJveHkiOiJzdGVlbWxpbmtlZC5hcHAiLCJ1c2VyIjoiYW5laWxwYXRlbCIsInNjb3BlIjpbImN1c3RvbV9qc29uIl0sImlhdCI6MTUzNDU3OTI0NywiZXhwIjoxNTM1MTg0MDQ3fQ.SskNKp8Uu00oBwfz7nUgtAIwRZX_3N_dux4wDpGhclM'], function(err, result) {
-      console.log(err, result);
-    })
-    this.busy.call( 'subscribe', ['aneilpatel'],function(err, result) {
-      console.log(err, result);
-    })
+    
     this.busy.subscribe( function(err, result) {
       console.log(err, result);
     })
    }
+   token:OAuth2Token = this.auth.token;
 
    getNotifs(){
+     this.busy.call('login', [this.token.access_token], function(err, result) {
+       console.log(err, result);
+      })
+      this.busy.call( 'subscribe', [this.token.username],function(err, result) {
+        console.log(err, result);
+      })
     
   }
   
