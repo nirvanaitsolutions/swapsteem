4540c0ab5efdc61a4bd1c2048e3de180626f25ce
aneilpatel05 <anil.patel.gty@gmail.com>
2018-08-22 01:19:37 -0700

    Added websockets

--- src/service/websockets.service.ts
+++ src/service/websockets.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
 import {WebSocketSubject,WebSocketSubjectConfig} from 'rxjs/observable/dom/WebSocketSubject';
+import * as busy from 'busyjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketsService {
-  public ws = new WebSocketSubject('wss://swapsteem-api.herokuapp.com');;
+  public busy = new busy.Client('wss://api.busy.org');;
 
   constructor() {
     
-    
-    this.ws.subscribe(
-      (data) => console.log(data),
-      (err) => console.error(err),
-      () => console.warn('Completed!')
-    )
+    this.busy.call('get_notifications', ['aneilpatel'], function(err, result) {
+      console.log(err, result);
+    })
+    this.busy.call('login', ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXBwIiwicHJveHkiOiJzdGVlbWxpbmtlZC5hcHAiLCJ1c2VyIjoiYW5laWxwYXRlbCIsInNjb3BlIjpbImN1c3RvbV9qc29uIl0sImlhdCI6MTUzNDU3OTI0NywiZXhwIjoxNTM1MTg0MDQ3fQ.SskNKp8Uu00oBwfz7nUgtAIwRZX_3N_dux4wDpGhclM'], function(err, result) {
+      console.log(err, result);
+    })
+    this.busy.call( 'subscribe', ['aneilpatel'],function(err, result) {
+      console.log(err, result);
+    })
+    this.busy.subscribe( function(err, result) {
+      console.log(err, result);
+    })
    }
 
+   getNotifs(){
+    
+  }
+  
 }
