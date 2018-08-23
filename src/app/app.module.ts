9bf2cd9f9b1bc9d3dec0291537164a5e95f8abb8
Ayush Agrawal <nitaandroid121@gmail.com>
2018-08-23 20:50:04 +0530

    remove angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { BuyComponent } from './buy/buy.component';
 import { SellComponent } from './sell/sell.component';
@@ -68,17 +67,6 @@ const config: SocketIoConfig = { url: 'http://swapsteem-api.herokuapp.com', opti
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
     HttpClientModule,
     SteemconnectModule.forRoot(environment.steemconnectConfig),
     CookieModule.forRoot(),
