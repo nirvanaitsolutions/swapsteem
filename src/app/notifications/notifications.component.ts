07eace0415a59b039899921ec1451d27d4c08efd
Ayush Agrawal <nitaandroid121@gmail.com>
2018-08-23 19:42:57 +0530

    Set Notification page and move ws service in notification

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import * as busy from 'busyjs'
+import { SteemconnectAuthService } from '../../app/steemconnect/services/steemconnect-auth.service';
+import {of} from 'rxjs'
+
+
+export interface OAuth2Token {
+  access_token: string;
+  expires_in: number;
+  username: string;
+}
 
 @Component({
   selector: 'app-notifications',
@@ -6,10 +17,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
+  
+  public busy = new busy.Client('wss://api.busy.org');
+   token:OAuth2Token = this.auth.token;
+   notifications :any = ['ayush', 'Agrawal'];
+   today: number = Date.now();
 
-  constructor() { }
+
+  constructor(private auth :SteemconnectAuthService) {
+    this.busy.subscribe( function(err, result) {
+      // TODO - Route data to notifications page
+      console.log(err, result);
+    })
+  }
 
   ngOnInit() {
+      this.busy.call('get_notifications', [this.token.username], (err, result)=> {
+        console.log("Notifications");
+        console.log(result);
+        this.notifications = result;
+      })
+
+      this.busy.call('login', [this.token.access_token], function(err, result) {
+       console.log(err, result);
+      })
+
+      this.busy.call( 'subscribe', [this.token.username],function(err, result) {
+        console.log(err, result);
+      })
+      this.busy.call('get_messages', [this.token.access_token,"0"], function(err, result) {
+       console.log(err, result);
+      })
   }
 
 }
