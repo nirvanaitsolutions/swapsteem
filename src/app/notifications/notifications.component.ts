f62a34feac2fd1aace4efd9ba539d5fadab5c93e
aneilpatel05 <anil.patel.gty@gmail.com>
2018-08-27 23:35:49 -0700

    notifs update

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import * as busy from 'busyjs'
 import { SteemconnectAuthService } from '../../app/steemconnect/services/steemconnect-auth.service';
 import {of} from 'rxjs'
+import { Notification } from './../module/notification';
 
 
 export interface OAuth2Token {
@@ -20,22 +21,58 @@ export class NotificationsComponent implements OnInit {
   
   public busy = new busy.Client('wss://api.busy.org');
    token:OAuth2Token = this.auth.token;
-   notifications :any = ['ayush', 'Agrawal'];
+   notifications :Notification[] =[] ;
    today: number = Date.now();
 
 
   constructor(private auth :SteemconnectAuthService) {
-    this.busy.subscribe( function(err, result) {
-      // TODO - Route data to notifications page
-      console.log(err, result);
-    })
+    
   }
 
   ngOnInit() {
+
+    this.busy.subscribe( function(err, result) {
+      if(result.type=='notification'){
+        
+        let notif:Notification={
+          type:'',
+          from:'',
+          text:'',
+          timestamp:0
+        };
+          notif.type=result.notification.type;
+          notif.from=result.notification.follower;
+          notif.text="You have a new "+result.notification.type;
+          notif.timestamp=result.notification.timestamp;
+          //console.log(this.notifications);
+          // TODO - Push new notif to array
+          //this.notifications.push(notif);
+          
+      }
+      // TODO - Route data to notifications page
+      console.log(err, result);
+
+    })
+
       this.busy.call('get_notifications', [this.token.username], (err, result)=> {
         console.log("Notifications");
         console.log(result);
-        this.notifications = result;
+        result.forEach(element => {
+          let notif:Notification={
+            type:'',
+            from:'',
+            text:'',
+            timestamp:0
+          };
+          console.log("element : "+element.type);
+          notif.type=element.type;
+          notif.from=element.follower;
+          notif.text="You have a new "+element.type;
+          notif.timestamp=element.timestamp;
+          this.notifications.push(notif);
+
+        });
+        //this.notifications = result;
       })
 
       this.busy.call('login', [this.token.access_token], function(err, result) {
