f36edffa8006cea658cf028216696c2eae7a696d
Aneilpatel05 <anil.patel.gty@gmail.com>
2019-03-18 10:01:56 -0400

    fix deploy

---
+++ src/environments/environment.=prod.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  SIGNUP_KEY: "2810.dgdgEWb9hiuHiii2ubhwavgbegtu21g3bG",
+  steemconnectConfig: {
+    clientId: "steemlinked.app",
+    scope: ['custom_json']
+  },
+  API_URL: "https://swapsteem-api.herokuapp.com",
+  REFERRER: "swapsteem",
+  SKIP_WHITE_LIST: true
+};
