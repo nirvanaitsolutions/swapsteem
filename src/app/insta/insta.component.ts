5b3276364770363ae5df3fdec454d569227f364e
Aneilpatel05 <anil.patel.gty@gmail.com>
2019-04-02 06:45:22 -0400

    added insta and landing page

---
+++ src/app/insta/insta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insta',
+  templateUrl: './insta.component.html',
+  styleUrls: ['./insta.component.css']
+})
+export class InstaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
