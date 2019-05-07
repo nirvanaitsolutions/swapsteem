47a5554ee2b379c65194f001a737a558991c9088
Neal Ray <godfather.php@gmail.com>
2019-05-07 16:45:15 +0530

    Buy Sweet page design and conversation rate updated

--- src/app/buy-sweet/buy-sweet.component.ts
+++ src/app/buy-sweet/buy-sweet.component.ts
@@ -3,14 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buy-sweet',
   templateUrl: './buy-sweet.component.html',
-  styleUrls: ['./buy-sweet.component.css'],
+  styleUrls: ['./buy-sweet.component.css']
 })
 export class BuySweetComponent implements OnInit {
+  conversionRate = 125;
+
   constructor() {}
 
   ngOnInit() {}
 
   submitForm(data) {
-    window.location.href = `https://app.steemconnect.com/?amount=${data.amount}&token=${data.amount*1.5}`;
+    window.location.href = `https://app.steemconnect.com/?amount=${
+      data.amount
+    }&token=${data.amount * this.conversionRate}`;
   }
 }
